30ed3753b3afcc3e3d6e3fc8edeff866ea663476
Rafael de Moura Speroni <rafaelsperoni@gmail.com>
2023-08-16 00:37:20 +0000

    aula 15ago

--- cursos.php
+++ cursos.php
@@ -1,15 +1,20 @@
 <?php
+#importa a classe
 require_once "Database.php";
 
+#instancia um obj de Database
 $db = new Database();
+#recebe a conexao
 $conexao = $db->connect();
 
-var_dump($conexao);
-
+#string de consulta
 $sql = "select * from curso";
-
+#executa a consulta
 $stmt = $conexao->query($sql);
-
+#transforma o resultado da consulta em um array de objs
 $cursos = $stmt->fetchAll(PDO::FETCH_OBJ);
 
+#exibe o conteudo do array
 var_dump($cursos);
+
+#pode percorrer com for, foreach
